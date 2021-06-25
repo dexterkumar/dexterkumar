5add2204dc3bcc713004c69bf93353ca1ee7f37c
dexterkumar <86490843+dexterkumar@users.noreply.github.com>
2021-06-25 14:12:16 -0400

    Add files via upload

---
+++ monthly_pay.py
@@ -0,0 +1,20 @@
+regular_hourly_pay = 46.6784
+extra_hourly_pay = 49.3064
+year  = int(12)
+
+
+def monthly_pay():
+    regular_pay = regular_hours * regular_hourly_pay
+    extra_pay = extra_hours * extra_hourly_pay
+    final_pay = regular_pay + extra_pay
+    yearly_estimate = year * regular_pay
+    print('Your pay for the month is', final_pay)
+    print('Yearly minimum with regular hours is',yearly_estimate)
+
+
+
+regular_hours = float(input('Enter the regular hours of the month \n '))
+extra_hours = float(input('Enter the extra hours of the month \n'))
+
+monthly_pay()
+
